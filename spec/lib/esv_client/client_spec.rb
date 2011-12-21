6a47e65e263a4e8de78cb7ae2f6b9ecd48a5b6b6
Perry Hertler <perry@hertler.org>
2011-12-21 09:58:51 -0600

    setting up search

--- spec/lib/esv_client/client_spec.rb
+++ spec/lib/esv_client/client_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 
 describe EsvClient::Client do
+  describe 'search' do
+    use_vcr_cassette :record => :all
 
+    let(:client) { EsvClient::Client.new }
+    let(:search_string) { "John 1:12-13" }
+
+    it "should successfully search" 
+  end
 end
 
